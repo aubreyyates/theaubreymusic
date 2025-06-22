9f16b761aaa51a2cbfa6011ebfc8312d0b9e2918
Aubrey Yates <aubreyyates999@hotmail.com>
2025-06-22 18:10:18 -0500

    Add height CSS to date display.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <Grid>
       <div id="home-container">
-        <Grid style={{ height: '1200px', position: 'relative' }}>
+        <Grid style={{ height: '1240px', position: 'relative' }}>
           <HomeOverlay></HomeOverlay>
         </Grid>
       </div>
