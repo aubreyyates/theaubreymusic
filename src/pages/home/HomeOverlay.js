4d18fae7bf946e7eb7285a17887f6438c21e8b6e
Aubrey Yates <aubreyyates999@hotmail.com>
2025-08-24 21:17:50 -0500

    Update dates.

--- src/pages/home/HomeOverlay.js
+++ src/pages/home/HomeOverlay.js
@@ -10,11 +10,11 @@ import ShapesLeft from './svgComponents/shapesLeft';
 let upcomingDates = [
   { text: `Elray's Live and Dive (Iowa, City, IA) 10/11/2025 8:00 PM - 9:30 PM`, link: `https://www.elraysliveanddive.com/` },
   { text: `Elray's Live and Dive (Iowa, City, IA) 09/05/2025 8:00 PM - 9:30 PM`, link: `https://www.elraysliveanddive.com/` },
-  { text: `Willow Creek Theatre (Iowa City, IA) 08/29/2025 8:00 PM`, link: `https://willowcreektheatre.org/` },
-  { text: `Short's Burgers Eastside (Iowa City, IA) 08/16/2025 6:30 PM - 8:30 PM`, link: `https://www.facebook.com/shortseastside/` }
+  { text: `Willow Creek Theatre (Iowa City, IA) 08/29/2025 8:00 PM`, link: `https://willowcreektheatre.org/` }
 ];
 
 let pastDates = [
+  `Short's Burgers Eastside (Iowa City, IA) 08/16/2025`,
   `The Sanctuary Pub (Iowa City, IA) 08/01/2025`,
   `Willow Creek Theatre (Iowa City, IA) 07/25/2025`,
   `Short's Burgers Eastside (Iowa City, IA) 07/03/2025`,
